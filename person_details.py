beb782b73a0f4ab92a546373c32d93e3b754c848
DuvvuruPavithra <duvvurupavithra28@gmail.com>
2022-03-04 16:52:53 +0530

    Added: Creating the person details using class

---
+++ person_details.py
@@ -0,0 +1,8 @@
+class Person:
+    def __init__(self, firstName, lastName, mobileNumber, State, City, pinCode):
+        self.firstName = firstName
+        self.lastName = lastName
+        self.mobileNumber = mobileNumber
+        self.State = State
+        self.City = City
+        self.pinCode = pinCode
